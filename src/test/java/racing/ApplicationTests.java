c27a3ecd55f469e3d70afee9d1e359ff3fb68bb2
Hyunjee <0lisa@naver.com>
2024-12-12 10:47:21 +0900

    차 생성 테스트

--- src/test/java/racing/ApplicationTests.java
+++ src/test/java/racing/ApplicationTests.java
@@ -3,10 +3,19 @@ package racing;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.*;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static racing.Car.*;
+import static racing.GameResult.*;
+
 class ApplicationTests {
 
     @Test
-    void test1() {
+    void playersTest() {
+        String test = "Hyundai,Porsche,Toyota";
+
+        System.out.println(players(test));
     }
 
 }
